9b982e7621f6cbf8f790920db4ace75894dbb9ed
Alexander Lesnichy <alexanderlesnichy@MacBook-Pro-Alexander.local>
2018-01-25 13:01:10 +0200

    hello git

--- HomeWork3/ViewController.swift
+++ HomeWork3/ViewController.swift
@@ -13,13 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        tryGit()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+        func tryGit() {
+            print("Hello GIThub!")
+        }
 
 }
-
